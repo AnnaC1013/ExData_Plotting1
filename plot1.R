e9f1aabffe8a0b656b470182efb6aa9555d6040e
Anna_C1013 <aecreighton1@gmail.com>
2015-06-06 21:19:59 -0400

    EDA Project 1 plots

---
+++ plot1.R
@@ -0,0 +1,13 @@
+##EDA Peer Assessment Project 1
+##Plot 1
+
+plot1 <- function() {
+  
+  hist(df$Global_active_power, main=paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot1.png", width=480, height=480)
+  dev.off()
+  cat("plot1.png has been saved in", getwd())
+}
+
+plot1()
+
